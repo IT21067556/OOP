d6d7e9c8443d5602e172961be852d849bebf37c5
Shifan M.S.M <88022521+IT21067556@users.noreply.github.com>
2022-02-28 18:01:58 +0530

    Add files via upload

---
+++ Tutorials/T3/Exercise1/Calculation.java
@@ -0,0 +1,9 @@
+public class Calculation {
+    static int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    static int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+}
---
+++ Tutorials/T3/Exercise1/DemoApp.java
@@ -0,0 +1,6 @@
+class DemoApp {
+    public static void main(String args[]) {
+        System.out.println(Calculation.add(13, 19));
+        System.out.println(Calculation.subtract(31, 19));
+    }
+}
